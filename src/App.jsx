2552ce5b08779d25e7a558da3484157a554e8af3
AnunaySharma2 <anunaysharma123456789@gmail.com>
2023-10-28 14:17:16 +0530

    add branch highlight, loading state and fix update section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import supabase from "./supabase";
 import {Link} from "react-router-dom";
 
@@ -6,6 +6,7 @@ function App() {
     const [name, setName] = useState("");
     const [employees, setEmployees] = useState([]);
     const [branch, setBranch] = useState("CSE");
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         async function fetchTodos() {
@@ -15,12 +16,14 @@ function App() {
                     .select('*')
                     .eq('dept', branch)
                     .ilike('name', `%${name}%`)
+                    .order('idd')
 
                 if (error) {
                     console.error('Error fetching data:', error);
                 } else {
                     setEmployees(data);
                 }
+                setLoading(false);
             } catch (error) {
                 console.error('Error fetching data:', error.message);
             }
@@ -30,35 +33,43 @@ function App() {
     }, [branch, name]);
 
     return (
-        <div className={"bg-gray-950 min-h-screen"}>
+        <div className="bg-gray-950 min-h-screen min-w-screen p-2">
             <input
                 type="text"
-                className={"bg-gray-900 rounded-md w-1/2 border-gray-950 mx-5 mt-5 mb-1"}
-                placeholder={"Search"}
+                className="bg-gray-900 rounded-md w-full md:w-1/2 border-gray-950 mx-5 mt-5 mb-1"
+                placeholder="Search"
                 onChange={(e) => setName(e.target.value)}
             />
-            <button className={"bg-yellow-500 hover:bg-yellow-600 py-2 text-gray-950"} onClick={() => {
-                if (name.length === 0) {
-                    alert("Please enter a valid name");
-                }
-            }}>
+            <button
+                className="bg-yellow-500 hover:bg-yellow-600 py-2 px-4 text-gray-950 block w-full md:w-auto md:inline-block my-2"
+                onClick={() => {
+                    if (name.length === 0) {
+                        alert("Please enter a valid name");
+                    }
+                }}>
+
                 Search
             </button>
-            <div className={"flex flex-1 p-5 gap-5"}>
+            <button
+                className="m-7 bg-gray-700 hover:bg-gray-800 text-white rounded-md shadow-md transition duration-200 px-4 py-2"
+            >
+                <Link to={"/admin"} className="text-white">Admin Panel</Link>
+            </button>
+            <div className="flex flex-wrap px-5 py-2 gap-5">
                 <button
-                    className={"bg-blue-900 hover:bg-blue-700"}
+                    className={`${branch === 'CSE' ? "bg-green-900 hover:bg-green-700" : "bg-blue-900 hover:bg-blue-700"}`}
                     onClick={() => setBranch("CSE")}
                 >
                     CSE
                 </button>
                 <button
-                    className={"bg-blue-900 hover:bg-blue-700"}
+                    className={`${branch === 'Mech' ? "bg-green-900 hover:bg-green-700" : "bg-blue-900 hover:bg-blue-700"}`}
                     onClick={() => setBranch("Mech")}
                 >
                     Mech
                 </button>
                 <button
-                    className={"bg-blue-900 hover:bg-blue-700"}
+                    className={`${branch === 'ECE' ? "bg-green-900 hover:bg-green-700" : "bg-blue-900 hover:bg-blue-700"}`}
                     onClick={() => setBranch("ECE")}
                 >
                     ECE
@@ -67,7 +78,15 @@ function App() {
 
 
             <div>
-                {employees.length === 0 && <div className="m-5 p-5 text-lg badge badge-error gap-2">
+                {loading && <div className="m-5 p-5 text-lg badge badge-error gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                         className="inline-block w-4 h-4 stroke-current">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                              d="M6 18L18 6M6 6l12 12"></path>
+                    </svg>
+                    Loading data...
+                </div>}
+                {(!loading && employees.length === 0) && <div className="m-5 p-5 text-lg badge badge-error gap-2">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                          className="inline-block w-4 h-4 stroke-current">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
@@ -75,22 +94,23 @@ function App() {
                     </svg>
                     No data present
                 </div>}
-                <div className={"flex"}>
+                <div className="flex flex-wrap">
                     {employees.map((employee) => (
-                        <div className="card mx-5 my-5 w-1/5 bg-gray-900 shadow-xl" key={employee.idd}>
-                            <figure><img className={"w-1/2 rounded-full mt-5"} src={employee.photo_url} alt="Shoes"/>
+                        <div className="card m-5 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 bg-gray-900 shadow-xl"
+                             key={employee.idd}>
+                            <figure className="px-10 pt-10">
+                                <img className="w-full rounded-full" src={employee.photo_url} alt="Employee"/>
                             </figure>
-                            <div className="card-body items-center">
+                            <div className="card-body items-center text-center">
                                 <h2 className="card-title font-bold text-2xl text-gray-50">{employee.name}</h2>
                                 <p className={"text-gray-200 font-semibold text-xl"}>{employee.dept}</p>
                                 <h3 className="card-title font-bold text-xl text-gray-50">ID {employee.idd}</h3>
                                 <Link to={`/employee/${employee.idd}`}>View Details</Link>
+
                             </div>
                         </div>
                     ))}
                 </div>
-
-
             </div>
         </div>
     );
--- src/EmployeeUpdate.jsx
+++ src/EmployeeUpdate.jsx
@@ -5,13 +5,16 @@ import {Link} from "react-router-dom";
 import supabase from "./supabase";
 import axios from "axios";
 
-function EmployeeUpdate() {
+function EmployeeUpdate(message) {
     const [id, setID] = useState();
     const [name, setName] = useState("");
     const [dept, setDept] = useState("");
+    const [updateName, setUpdateName] = useState("");
+    const [updateDept, setUpdateDept] = useState("");
     const [imageURL, setImageURL] = useState("");
+    const [updateImageURL, setUpdateImageURL] = useState("");
 
-    const handleImageUpload = async (e) => {
+    const handleImageUploadUpdate = async (e) => {
         const formData = new FormData();
         formData.append('image', e.target.files[0]);
 
@@ -32,29 +35,26 @@ function EmployeeUpdate() {
             if (response.status === 200) {
                 const imageUrl = response.data.data.url;
                 console.log('Image URL:', imageUrl);
-                setImageURL(imageUrl)
+                setUpdateImageURL(imageUrl)
+
+                setTimeout(() => {}, 1000);
+
+                const {data, error} = await supabase
+                    .from("employees")
+                    .update({photo_url: updateImageURL})
+                    .eq('idd', id)
+                    .select()
+
+                if (error) {
+                    alert("Error updating name")
+                    return;
+                }
+                alert("Updated successfully")
             }
         } catch (error) {
             console.error('Error uploading image:', error);
         }
     };
-
-    async function valueExistsInColumn(valueToCheck) {
-        try {
-            const {data, error} = await supabase
-                .from("employees")
-                .select("*")
-                .eq("idd", valueToCheck)
-                .limit(1);  // Just need to check existence, so limiting results to 1.
-
-            if (error) throw error;
-
-            return data.length > 0;  // Return true if value exists, else false.
-        } catch (err) {
-            console.error("Error checking value:", err);
-            return false;
-        }
-    }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -64,79 +64,125 @@ function EmployeeUpdate() {
             return;
         }
 
-        if (dept && (dept !== "CSE" && dept !== "ECE" && dept !== "Mech")) {
-            alert("Department must be CSE, ECE or Mech");
+        try {
+            const {data, error} = await supabase
+                .from("employees")
+                .select("*")
+                .eq("idd", id)
+                .limit(1);
+
+            if (error) throw error;
+            if (data.length === 0) {
+                alert("Employee with this ID does not exist in the table")
+            } else {
+                setName(data[0]["name"]);
+                setDept(data[0]["dept"]);
+                setImageURL(data[0]["photo_url"])
+            }
+        } catch (err) {
+            alert("Error checking value:", err);
+        }
+    };
+
+    async function nameChangeHandler() {
+        if (updateName === "") {
+            alert("Name cannot be empty");
             return;
         }
-
-        if (await valueExistsInColumn(id) === false) {
-            alert("A row with given ID does not exist in the table");
-            return;
-        }
-
-        // const updatedFeatures = {};
-        // if(name){
-        //     updatedFeatures
-        // }
-        const updates = {};
-        if (name) {
-            updates.name = name;
-        }
-        if (dept) {
-            updates.dept = dept;
-        }
-        if (imageURL) {
-            updates.photoURL = imageURL;
-        }
-
         const {data, error} = await supabase
             .from("employees")
-            .update(updates)
+            .update({name: updateName})
             .eq('idd', id)
             .select()
 
         if (error) {
-            alert("Error update data");
+            alert("Error updating name")
+            return
+        }
+        alert("Updated successfully")
+        setID(null);
+        setName("");
+        setDept("");
+    }
+
+    async function deptChangeHandler() {
+        if (updateDept !== "CSE" && updateDept !== "ECE" && updateDept !== "Mech") {
+            alert("Department must be CSE, ECE or Mech");
             return;
         }
 
-        alert("Successfully updated data");
-        setID();
-        setName('');
-        setDept('');
-    };
+        const {data, error} = await supabase
+            .from("employees")
+            .update({dept: updateDept})
+            .eq('idd', id)
+            .select()
+
+        if (error) {
+            alert("Error updating name")
+            return
+        }
+        alert("Updated successfully")
+        setID(null);
+        setName("");
+        setDept("");
+    }
 
     return (
-        <div className={styles.container}>
-            <h1 className={"text-white text-2xl mb-6 font-semibold"}>Update Data</h1>
+        <div className={"bg-gray-950 min-h-screen min-w-screen p-5"}>
+            <h1 className={"text-white text-3xl mb-6 font-semibold"}>Update Data</h1>
             <form onSubmit={handleSubmit}>
-                <div className={styles.form_group}>
+                {!name && <div className={styles.form_group}>
                     <label>Update ID:</label>
-                    <input className={"text-white bg-gray-900 border-none my-2"} type="number" value={id}
+                    <input className={"text-white w-1/5 bg-gray-900 border-none my-2"} type="number"
                            onChange={(e) => setID(e.target.value)}/>
-                </div>
-                <div className={styles.form_group}>
-                    <label>Update Name:</label>
-                    <input className={"text-white bg-gray-900 border-none my-2"} type="text" value={name}
-                           onChange={(e) => setName(e.target.value)}/>
-                </div>
-                <div className={styles.form_group}>
-                    <label>Update Branch:</label>
-                    <input className={"text-white bg-gray-900 border-none my-2"} type="text" value={dept}
-                           onChange={(e) => setDept(e.target.value)}/>
-                </div>
-                <div className={styles.form_group}>
-                    <label>Update Photo URL:</label>
-                    <input type="file"
-                           className="text-white w-full border-none bg-gray-800 my-2 px-3 py-2 rounded-md text-black placeholder-gray-600"
-                           accept={"image/*"}
-                           onChange={handleImageUpload}/>
-                </div>
+                </div>}
+
+                {/*<div className={styles.form_group}>*/}
+                {/*    <label>Update Name:</label>*/}
+                {/*    <input className={"text-white bg-gray-900 border-none my-2"} type="text" value={name}*/}
+                {/*           onChange={(e) => setName(e.target.value)}/>*/}
+                {/*</div>*/}
+                {/*<div className={styles.form_group}>*/}
+                {/*    <label>Update Branch:</label>*/}
+                {/*    <input className={"text-white bg-gray-900 border-none my-2"} type="text" value={dept}*/}
+                {/*           onChange={(e) => setDept(e.target.value)}/>*/}
+                {/*</div>*/}
+                {/*<div className={styles.form_group}>*/}
+                {/*    <label>Update Photo URL:</label>*/}
+                {/*    <input type="file"*/}
+                {/*           className="text-white w-full border-none bg-gray-800 my-2 px-3 py-2 rounded-md text-black placeholder-gray-600"*/}
+                {/*           accept={"image/*"}*/}
+                {/*           onChange={handleImageUploadUpdate}/>*/}
+                {/*</div>*/}
+                {name && <div>
+                    <div>
+                        <img className="w-1/5 h-1/5 py-3 rounded-full" src={imageURL} alt="Employee"/>
+                    </div>
+                    <div>
+                        <input
+                            className={"text-white w-1/5 border-none bg-gray-800 my-3 rounded-md  placeholder-gray-400"}
+                            placeholder={`${name} (Type here to update)`}
+                            onChange={(e) => setUpdateName(e.target.value)}></input>
+                        <button className={"p-2 mx-3"} onClick={nameChangeHandler}>Update Name</button>
+                    </div>
+                    <div>
+                        <input
+                            className={"text-white w-1/5 border-none bg-gray-800 my-3 rounded-md placeholder-gray-400"}
+                            placeholder={`${dept} (Type here to update)`}
+                            onChange={(e) => setUpdateDept(e.target.value)}></input>
+                        <button className={"p-2 mx-3"} onClick={deptChangeHandler}>Update dept</button>
+                    </div>
+                    <div>
+                        <input type="file"
+                               className="text-white w-1/5 border-none bg-gray-800 my-2 px-3 py-2 rounded-md placeholder-gray-600"
+                               accept={"image/*"} onChange={handleImageUploadUpdate} placeholder={"Update Image"}/>
+                    </div>
+                </div>}
                 <div className="mb-4">
-                    <button type="submit"
-                            className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md shadow-md transition duration-200">
+                    {!name && <button type="submit"
+                                      className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md shadow-md transition duration-200">
                         Submit
-                    </button>
+                    </button>}
                 </div>
                 <button
                     className="bg-gray-700 hover:bg-gray-800 text-white py-2 px-4 rounded-md shadow-md transition duration-200">
